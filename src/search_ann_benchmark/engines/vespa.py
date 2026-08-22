9caa4f19cea09b28b578d758cec657f4729b088d
Shinsuke Sugaya <shinsuke@apache.org>
2026-08-22 11:41:49 +0900

    chore(vespa): bump Vespa version from 8.687.75 to 8.738.17 (#52)

--- src/search_ann_benchmark/engines/vespa.py
+++ src/search_ann_benchmark/engines/vespa.py
@@ -30,7 +30,7 @@ class VespaConfig(EngineConfig):
     name: str = "vespa"
     host: str = "localhost"
     port: int = 8090
-    version: str = "8.687.75"
+    version: str = "8.738.17"
     container_name: str = "benchmark_vespa"
     management_port: int = 19081
 
